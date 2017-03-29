1d2d90bbacc3c5ee24e92b229220a626a047b728
AIR\adam.mugnaini <adam.mugnaini@aarcorp.com>
2017-03-29 15:09:30 -0500

    Definition to elipse.js

--- elipse.js
+++ elipse.js
@@ -1,9 +1,35 @@
 "use-strict"
 
-let str1 = "1234567890";
+/*
+This was a technical question that came up in an interview.
+I got it half correct, but forgot to include the prototype portion.
+*/
+
+/*
+Question:
+	How would you write code to elipse (or truncate) a string?
+
+Setup:
+	let name ="1234567890"
+
+	name.elipse(8);
+
+	elipse {
+		...
+	}
+
+My Initial Anser:
+	function elipse(x) {
+		return this.substring(0, x);
+	}
+
+*/
+
+// Correct Answer
+let name = "1234567890";
 
 String.prototype.elipse = function (x) {
 	return this.substring(0, x);
 }
 
-console.log(str1.elipse(8));
+console.log(name.elipse(8));
