ba335d7f7cb6f5d3861896c4043b78df3ea98f8a
NullGemini <nullgemini@me.com>
2021-11-08 23:19:38 -0600

    fixes

--- currency_exchange_1.js
+++ currency_exchange_1.js
@@ -5,7 +5,7 @@ var options = {
   url: 'https://currency-exchange.p.rapidapi.com/listquotes',
   headers: {
     'x-rapidapi-host': 'currency-exchange.p.rapidapi.com',
-    'x-rapidapi-key': 'bafe5d1b21msh588f061f55a9bf5p1ee83cjsn63e72ce6b4e8'
+    'x-rapidapi-key': ''
   }
 };
 
